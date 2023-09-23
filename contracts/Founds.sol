77e92b86b0f1f90398ed9a868deb007c49fe9b13
Guillermo Tala <guille86598@gmail.com>
2023-09-22 22:05:50 -0400

    first version

---
+++ contracts/Founds.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Founds {
+
+    constructor(){
+    }
+
+
+    function createCampaign() public {
+
+    }
+
+
+    function participate() payable public {
+
+    }
+
+
+
+}
