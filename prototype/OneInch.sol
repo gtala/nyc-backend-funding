e03352f9db00c63dc8c5e5ba39f7df0e2171dfb1
Luis Dominguez <ld@luis.sh>
2023-09-24 11:00:25 -0400

    Adding a straggler prototype for the sake of acknowledging existence

---
+++ prototype/OneInch.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.0;
+
+contract OneInchTest {
+
+    address public ONEINCH_ADDRESS;
+
+    constructor(_oneInchAddress) {
+        ONEINCH_ADDRESS = _oneInchAddress;
+    }
+/**
+ * See: https://help.1inch.io/en/articles/6909554-how-to-stake-1inch-tokens-and-delegate-unicorn-power
+ */
+
+  //Trade ETH for 1INCH
+
+  //Stake 1INCH for Unicorn Power
+  
+  //Delegate 1INCH to resolver
+}
